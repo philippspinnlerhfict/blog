54b68adb844bdf619634abf53abd801f996f9fe3
Philipp Spinnler <p.spinnler@hf-ict.ch>
2020-12-01 17:20:04 +0100

    Woche 6

---
+++ src/main/java/ch/hfict/springboot/model/UserDto.java
@@ -0,0 +1,23 @@
+package ch.hfict.springboot.model;
+
+
+public class UserDto {
+    String username;
+    String password;
+
+    public String getUsername() {
+        return username;
+    } 
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
